71d145d4409800285edf0f7127af9c981ed4b719
Patrick McMahon <pmcmahon14@gmail.com>
2019-04-05 16:34:20 -0700

    now dynamically output and calculate ingredients

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Fragment from '../../hoc/Fragment';
 import Burger from '../../Components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            lettuce: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    };
+
     render() {
         return (
             <Fragment>
-                <Burger/>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
 
             </Fragment>
