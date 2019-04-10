cd4de9e92544cc5ea1ae31773fd6e39ce7e7da68
Patrick McMahon <pmcmahon14@gmail.com>
2019-04-09 20:18:19 -0700

    added backdrop to sidebar and it can now close

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Fragment from '../../hoc/Fragment';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = (props) => (
-    <Fragment>
-        <Toolbar/>
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Fragment>
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    };
+
+    SideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    };
+
+    render () {
+        return (
+            <Fragment>
+            <Toolbar/>
+            <SideDrawer
+                open={this.state.showSideDrawer}
+                closed={this.SideDrawerClosedHandler}/>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Fragment>
+        )
+    }
+}
 
 export default Layout;
--- src/Components/Navigation/SideDrawer/SideDrawer.js
+++ src/Components/Navigation/SideDrawer/SideDrawer.js
@@ -3,10 +3,18 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Fragment from '../../../hoc/Fragment';
 
 const SideDrawer = (props) => {
+    let attachedClasses = [classes.SideDrawer, classes.Close];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+    }
     return (
-        <div className={classes.SideDrawer}>
+        <Fragment>
+        <Backdrop show={props.open} clicked={props.closed}/>
+        <div className={attachedClasses.join(' ')}>
             <div className={classes.Logo}>
                 <Logo/>
             </div>
@@ -14,6 +22,7 @@ const SideDrawer = (props) => {
                 <NavigationItems/>
             </nav>
         </div>
+        </Fragment>
     );
 };
 
