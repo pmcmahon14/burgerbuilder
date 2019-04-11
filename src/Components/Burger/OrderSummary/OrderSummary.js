0d0f40ed722c051fd100039c5564e8d760d43399
Patrick McMahon <pmcmahon14@gmail.com>
2019-04-11 10:28:28 -0700

    improved performance in BurgerBuilder, Modal, OrderSummary

--- src/Components/Burger/OrderSummary/OrderSummary.js
+++ src/Components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Fragment from '../../../hoc/Fragment';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-        return (
-            <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-            </li> );
-    });
-    return (
-        <Fragment>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Fragment>
-    )
-};
+class OrderSummary extends Component {
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}>
+                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                    </li> );
+            });
+        return(
+            <Fragment>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button btnType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Fragment>
+        );
+    }
+}
 
 export default OrderSummary;
--- src/Components/UI/Modal/Modal.js
+++ src/Components/UI/Modal/Modal.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import classes from './Modal.module.css';
 import Fragment from '../../../hoc/Fragment';
 import Backdrop from '../Backdrop/Backdrop';
 
-const Modal = (props) => (
-    <Fragment>
-        <Backdrop show={props.show} clicked={props.modalClosed}/>
-    <div
-        className={classes.Modal}
-    style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1': '0'
-    }}>
-        {props.children}
-    </div>
-    </Fragment>
-);
+class Modal extends Component {
+    render () {
+        return (
+            <Fragment>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div
+                    className={classes.Modal}
+                    style={{
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1': '0'
+                    }}>
+                    {this.props.children}
+                </div>
+            </Fragment>
+        )
+    }
+}
 
 export default Modal;
