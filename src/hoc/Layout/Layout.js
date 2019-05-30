3a6a0ee40488bf9b676c037d316b851b45eb2a32
Patrick McMahon <pmcmahon14@gmail.com>
2019-05-30 15:34:58 -0700

    updated UI to log in depending on state

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
 import Fragment from '../Fragment/Fragment';
 import classes from './Layout.module.css';
 import Toolbar from '../../Components/Navigation/Toolbar/Toolbar';
@@ -22,8 +24,11 @@ class Layout extends Component {
     render () {
         return (
             <Fragment>
-            <Toolbar drawerToggleClicked={this.SideDrawerToggleHandler}/>
+            <Toolbar
+                isAuth={this.props.isAuthenticated}
+                drawerToggleClicked={this.SideDrawerToggleHandler}/>
             <SideDrawer
+                isAuth={this.props.isAuthenticated}
                 open={this.state.showSideDrawer}
                 closed={this.SideDrawerClosedHandler}/>
             <main className={classes.Content}>
@@ -34,4 +39,10 @@ class Layout extends Component {
     }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    }
+};
+
+export default connect(mapStateToProps)(Layout);
