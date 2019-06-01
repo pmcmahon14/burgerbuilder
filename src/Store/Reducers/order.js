7b272d7c0a587959f37543f6deb13d68656fd8c6
Patrick McMahon <pmcmahon14@gmail.com>
2019-05-31 21:50:07 -0700

    using updateObject in entire app

--- src/Store/Reducers/order.js
+++ src/Store/Reducers/order.js
@@ -1,5 +1,5 @@
 import * as actionTypes from '../Actions/actionTypes';
-import {updateObject} from "../utility";
+import {updateObject} from "../../Shared/utility";
 
 const initialState = {
     orders: [],
