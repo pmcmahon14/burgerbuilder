3c0dadd1ea3a2166ff8466ffbe3be93cff899f27
Patrick McMahon <pmcmahon14@gmail.com>
2019-06-08 13:29:26 -0700

    more logic to saga

--- src/Store/Sagas/Auth.js
+++ src/Store/Sagas/Auth.js
@@ -1,12 +1,17 @@
+import {delay} from 'redux-saga/effects';
 import {put} from 'redux-saga/effects';
 
-import * as actionTypes from "../Actions/actionTypes";
+import * as actions from "../Actions/index";
 
 export function* logoutSaga(action) {
     yield localStorage.removeItem('token');
     yield localStorage.removeItem('expirationDate');
     yield localStorage.removeItem('userId');
-    yield put ({
-        type: actionTypes.AUTH_LOGOUT
-    })
+    yield put (actions.logoutSucceed());
+}
+
+export function* checkAuthTimeoutSaga(action) {
+    yield delay(+action.expirationTime*1000);
+    yield put(actions.logout());
+
 }
--- src/Store/Sagas/index.js
+++ src/Store/Sagas/index.js
@@ -1,8 +1,10 @@
 import {takeEvery} from 'redux-saga/effects';
 
 import * as actionTypes from '../Actions/actionTypes';
-import {logoutSaga} from "./Auth";
+import {logoutSaga, checkAuthTimeoutSaga} from "./Auth";
 
 export function* watchAuth() {
     yield takeEvery(actionTypes.AUTH_INITIATE_LOGOUT, logoutSaga);
+    yield takeEvery(actionTypes.AUTH_CHECK_TIMEOUT, checkAuthTimeoutSaga);
+
 }
